5d0c0881202d0166a931ed20a99fd556e832ad3c
windowssed <69721049+windowssed@users.noreply.github.com>
2026-08-06 01:38:44 +0800

    Switch to Astrofolio theme and localize to Chinese

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export const metaData = {
+  baseUrl: "https://www.dsyou.cn/",
+  title: "windowssed",
+  name: "windowssed",
+  ogImage: "/opengraph-image.png",
+  description: "windowssed 的个人博客，记录生活、学习与思考。",
+};
+
+export const socialLinks = {
+  twitter: "",
+  github: "https://github.com/windowssed",
+  instagram: "",
+  linkedin: "",
+  email: "",
+};
